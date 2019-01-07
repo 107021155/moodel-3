6b31a8f2ee2e90d872034e73642481abc55d5c61
107021155 <43198527+107021155@users.noreply.github.com>
2019-01-07 16:20:52 +0800

    Add files via upload

---
+++ 費式數列.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println(fun(scn.nextInt()));
+        }
+        public static int fun(int n){
+        if (n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }else{
+            return fun(n-1)+fun(n-2);
+        }
+        }
+    }
